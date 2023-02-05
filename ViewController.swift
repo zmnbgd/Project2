2355dd3100645f03774ad40412293eeae24c27a9
Marko Živanović <32281586+zmnbgd@users.noreply.github.com>
2023-02-06 00:06:08 +0100

    Add files via upload

---
+++ ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  Project2
+//
+//  Created by Marko Zivanovic on 5.2.23..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    //MARK: Properties
+    var countries = [String]()
+    var score = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+        
+        button1.layer.borderWidth = 0.5
+        button2.layer.borderWidth = 0.5
+        button3.layer.borderWidth = 0.5
+        
+        button1.layer.borderColor = UIColor.lightGray.cgColor
+        button2.layer.borderColor = UIColor.lightGray.cgColor
+        button3.layer.borderColor = UIColor.lightGray.cgColor
+        
+        askQuestion()
+    }
+    func askQuestion() {
+        button1.setImage(UIImage(named: countries[0]), for: .normal)
+        button2.setImage(UIImage(named: countries[1]), for: .normal)
+        button3.setImage(UIImage(named: countries[3]), for: .normal)
+    }
+}
+
