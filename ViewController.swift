a5b505b0e3c36eada10fb8eb5ea2451e10e20ed2
Marko Živanović <32281586+zmnbgd@users.noreply.github.com>
2023-02-07 00:00:48 +0100

    Add files via upload

--- ViewController.swift
+++ ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
     //MARK: Properties
     var countries = [String]()
     var score = 0
+    var correctAnswer = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,12 +30,30 @@ class ViewController: UIViewController {
         button2.layer.borderColor = UIColor.lightGray.cgColor
         button3.layer.borderColor = UIColor.lightGray.cgColor
         
+        //askQuestion(action: nil)
         askQuestion()
     }
-    func askQuestion() {
+    func askQuestion(action: UIAlertAction! = nil) {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
         button1.setImage(UIImage(named: countries[0]), for: .normal)
         button2.setImage(UIImage(named: countries[1]), for: .normal)
         button3.setImage(UIImage(named: countries[3]), for: .normal)
+        title = countries[correctAnswer].uppercased()
+    }
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        var title: String
+        if sender.tag == correctAnswer {
+            title = "Correct answer"
+            score += 1
+        } else {
+            title = "Wrong aswer"
+            score -= 1
+        }
+        let ac = UIAlertController(title: title, message: "Your score is\(score)", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+        present(ac, animated: true)
     }
 }
 
